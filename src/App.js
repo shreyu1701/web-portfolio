bac4c2207b0c2fafb38331923a697fd097796dcc
Shreyansh Koladiya <55000207+shreyu1701@users.noreply.github.com>
2025-01-24 16:07:35 -0500

    Added: Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./pages/home";
 import Chatbot from "./hooks/chatbot";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div>
+      <Analytics />
       <Home />
       <Chatbot />
     </div>
